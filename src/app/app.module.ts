ead8c7a4f072c5e1f7d8558ab8bddc4b35616e13
SaurabhRane1702 <63310412+SaurabhRane1702@users.noreply.github.com>
2022-07-05 00:11:27 +0530

    Reactive forms and start of services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { RestauaurantAppComponent } from './restauaurant-app/restauaurant-app.component';
 import { RestaurantDashComponent } from './restaurant-dash/restaurant-dash.component';
 
@@ -10,13 +12,16 @@ import { RestaurantDashComponent } from './restaurant-dash/restaurant-dash.compo
   declarations: [
     AppComponent,
     RestauaurantAppComponent,
-    RestaurantDashComponent
+    RestaurantDashComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
