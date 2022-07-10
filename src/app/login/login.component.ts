130dba85c1fc866c7f4983d1e9c545566e8337f4
SaurabhRane1702 <63310412+SaurabhRane1702@users.noreply.github.com>
2022-07-10 20:09:47 +0530

    Login Form Complete

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,49 @@
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm!: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private _http: HttpClient,
+    private _router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: [''],
+    });
   }
 
+  //Login The User to Resturant Dashboard
+  logIn() {
+    this._http.get<any>('http://localhost:3000/sign-up').subscribe(
+      (res) => {
+        const user = res.find((a: any) => {
+          return (
+            a.email === this.loginForm.value.email &&
+            a.password === this.loginForm.value.password
+          );
+        });
+        if (user) {
+          alert('Login Succesfully');
+          this.loginForm.reset();
+          this._router.navigate(['/restaurant-dashboard']);
+        } else {
+          alert('User Not Found! Please Check Your credecntials');
+        }
+      },
+      (err) => {
+        alert('Something went wrong while Login!');
+      }
+    );
+  }
 }
